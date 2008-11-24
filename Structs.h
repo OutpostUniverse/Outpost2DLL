06d971adf599c85b685bd2b97edba5d1b5d12994
Hooman <Hooman@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2008-11-24 07:10:17 +0000

    Updated Structs.h -- updated field info
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/API/Outpost2DLL@138 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

--- Structs.h
+++ Structs.h
@@ -1,4 +1,9 @@
 
+
+enum UnitClassifications;
+
+
+
 // Note: This file contains all the exported structures from Outpost2.exe.
 // Note: Some of these structures are really more like full classes but
 //		 since they called them struct's we'll let that one slide. =)
@@ -13,13 +18,14 @@ struct OP2 LOCATION
 {
 public:
 	LOCATION(int tileX, int tileY)	{ this->x = tileX; this->y = tileY; };
-	LOCATION()						{ this->x = 0; this->y = 0; };
+	LOCATION();						// { this->x = 0; this->y = 0; };
 	struct LOCATION & operator = (struct LOCATION const &);
 	void Add(struct LOCATION const &vector);
 	void Clip();
 	static struct LOCATION __fastcall Difference(struct LOCATION const &firstOperand, struct LOCATION const &secondOperand);
 	int Norm();
 
+public:
 	int x, y;
 };
 
@@ -30,7 +36,7 @@ public:
 	MAP_RECT(int tileX1, int tileY1, int tileX2, int tileY2); // Top left: (tileX1, tileY1), Bottom right: (tileX2, tileY2)
 	MAP_RECT();
 	struct MAP_RECT & operator = (struct MAP_RECT const &);
-	int Check(struct LOCATION &ptToCheck);
+	int Check(struct LOCATION &ptToCheck);		// int bInRect  [Checks if the point is in the rect  [handles x wrap around for rect coordinates]]
 	void ClipToMap();
 	void FromPtSize(struct LOCATION const &, struct LOCATION const &);
 	int Height() const;
@@ -40,6 +46,7 @@ public:
 	struct LOCATION Size() const;
 	int Width() const;
 
+public:
 	int x1, y1, x2, y2;		// Top left: (x1, y1), Bottom right: (x2, y2)
 };
 
@@ -70,23 +77,47 @@ struct OP2 MrRec
 
 struct OP2 PWDef 
 {
-	int x; // -1 to terminate list
-	int y;
+	int x1; // -1 to terminate list
+	int y1;
+	int x2;
+	int y2;
+	int x3;
+	int y3;
+	int x4;
+	int y4;
+	int time1;
+	int time2;
+	int time3;
 };
 
+// CommandPackets seem to have a set maximum of 0x74 bytes
+// Note: The compiler must be told to pack this structure since the 
+//		 short dataLength would otherwise have 2 padding bytes after
+//		 it which would mess up the rest of the structure.
+/*
+#pragma pack(push, 1)
 struct OP2 CommandPacket 
 {
+	int type;				// Type of command - see secret list :)
+	short dataLength;		// Length of dataBuff
+	int timeStamp;			// Game Tick (only seems to be used for network traffic)
+	int unknown;			// **TODO** figure this out (only used for network traffic?)
+	char dataBuff[0x66];	// Dependent on message type
 };
+#pragma pack(pop)
+*/
 
+// Size: 0x20  [0x20 = 32, or 8 dwords]  [Note: last 2 fields are shorts]
 struct OP2 UnitRecord 
 {
-	map_id Type;
-	int X, Y;
-	int unknown1, unknown2;
-	map_id Weapon;
-	int unknown3;
-	short Cargo;
-	short Amount;
+	map_id unitType;							// 0x0
+	int x;										// 0x4
+	int y;										// 0x8
+	int unknown1;								// 0xC  ** [unused?]
+	int rotation;								// 0x10 ** [Byte?]
+	map_id weaponType;							// 0x14
+	UnitClassifications unitClassification;		// 0x18
+	short cargoType;							// 0x1C
+	short cargoAmount;							// 0x1E
 };
-
 
