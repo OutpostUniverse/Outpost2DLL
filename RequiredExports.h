55557cb8e6c4420c443b71eecc5c6625992345ed
Hooman <Hooman@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2008-11-24 07:32:02 +0000

    Breaking Change: RequiredExports.h -- Renamed SDescBlock to AIModDesc, however, a compatability typedef was added. Added an "Extern" macro. Changed level exports to use Extern macro, and marked as deprecated. Additional AIModDesc fields defined. Extra level export macros added for convenience.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/API/Outpost2DLL@143 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

--- RequiredExports.h
+++ RequiredExports.h
@@ -6,6 +6,7 @@
 
 // A define used to make exporting functions and variables easier
 #define SCRIPT_API extern "C" __declspec(dllexport)
+#define Export extern "C" __declspec(dllexport)
 
 
 // missionType defines (and prefix needed on the DLL name)
@@ -13,25 +14,49 @@
 //		 which corresponds to the level number.
 enum MissionTypes
 {
-	Colony					= -1, //0xFFFFFFFF	// c
-	AutoDemo				= -2, //0xFFFFFFFF	// a
-	Tutorial				= -3, //0xFFFFFFFD	// t
+	Colony					= -1, //0xFF	// c
+	AutoDemo				= -2, //0xFF	// a
+	Tutorial				= -3, //0xFD	// t
 
-	MultiLandRush			= -4, //0xFFFFFFFC	// mu(x)
-	MultiSpaceRace			= -5, //0xFFFFFFFB	// mf(x)
-	MultiResourceRace		= -6, //0xFFFFFFFA	// mr(x)
-	MultiMidas				= -7, //0xFFFFFFF9	// mm(x)
-	MultiLastOneStanding	= -8, //0xFFFFFFF8	// ml(x)
+	MultiLandRush			= -4, //0xFC	// mu(x)
+	MultiSpaceRace			= -5, //0xFB	// mf(x)
+	MultiResourceRace		= -6, //0xFA	// mr(x)
+	MultiMidas				= -7, //0xF9	// mm(x)
+	MultiLastOneStanding	= -8, //0xF8	// ml(x)
 };
 
 
 // Structure for important data exports needed for OP2 to recognize the level
-struct SDescBlock{
+struct AIModDesc
+{
+	// Important level details
 	int missionType;			// Mission type (defined above) or mission number (positive values) for campaign games
 	int numPlayers;				// Number of players (on a multipalyer map)
 	int maxTechLevel;			// Maximum tech level (Set to 12 to enable all techs)
 	int boolUnitMission;		// Set to 1 to disable most reports (suitable for unit-only missions)
+	// Extra baggage that doesn't need to be set properly
+	char* mapName;
+	char* levelDesc;
+	char* techtreeName;
+	int checksum;
 };
+
+// ** Debug **  (Deprecated)
+typedef AIModDesc SDescBlock;
+
+
+#define ExportLevelDetails(levelDesc, mapName, techTreeName, missionType, numPlayers) \
+	Export char MapName[] = mapName; \
+	Export char LevelDesc[] = levelDesc; \
+	Export char TechtreeName[] = techTreeName; \
+	Export AIModDesc DescBlock = { missionType, numPlayers, (missionType > 0) ? missionType : 12, false, MapName, LevelDesc, TechtreeName, 0 }; \
+
+#define ExportLevelDetailsEx(levelDesc, mapName, techTreeName, missionType, numPlayers, maxTechLevel, bUnitOnlyMission) \
+	Export char MapName[] = mapName; \
+	Export char LevelDesc[] = levelDesc; \
+	Export char TechtreeName[] = techTreeName; \
+	Export AIModDesc DescBlock = { missionType, numPlayers, maxTechLevel, bUnitOnlyMission, MapName, LevelDesc, TechtreeName, 0 };
+
 
 // Structure for specifying DLL specific data to write to saved game files.
 // It is used as a return parameter in the GetSaveRegions export.
@@ -48,10 +73,11 @@ struct BufferDesc
 //		 structure is also exported to give additional mission info
 //		 (some of which corresponds to the naming of the DLL).
 
-SCRIPT_API char MapName[];			// Holds the file name of the .map file
-SCRIPT_API char LevelDesc[];		// Description that appears in the list/menu 
-SCRIPT_API char TechtreeName[];		// The tech tree to use for this level
-SCRIPT_API SDescBlock DescBlock;	// Exports game info
+// ** DEBUG **  (Deprecated)
+Export char MapName[];			// Holds the file name of the .map file
+Export char LevelDesc[];		// Description that appears in the list/menu 
+Export char TechtreeName[];		// The tech tree to use for this level
+Export AIModDesc DescBlock;		// Exports game info
 
 
 // Note: These functions are required exports from all level DLLs.
@@ -62,12 +88,13 @@ SCRIPT_API SDescBlock DescBlock;	// Exports game info
 //		 this function is usually left empty) and specifying the data that
 //		 needs to be stored to saved game files.
 
-SCRIPT_API int  __cdecl InitProc();			// Called on mission startup
-SCRIPT_API void __cdecl AIProc();			// Can be used to implement an AI (Called every game cycle)
-SCRIPT_API void __cdecl GetSaveRegions(struct BufferDesc &bufDesc);	// Used to return a buffer description to OP2 for storing data to saved game files
+// ** DEBUG **  (Deprecated)
+Export int  __cdecl InitProc();			// Called on mission startup
+Export void __cdecl AIProc();			// Can be used to implement an AI (Called every game cycle)
+Export void __cdecl GetSaveRegions(struct BufferDesc &bufDesc);	// Used to return a buffer description to OP2 for storing data to saved game files
 
 // Note: The following two exports are not strictly required but are
 //		 usually found in all level DLLs.
-SCRIPT_API int  __cdecl StatusProc();			// Unused, returns 0
-SCRIPT_API void __cdecl NoResponseToTrigger();	// Used for triggers with no custom effects
+//Export int  __cdecl StatusProc();			// Unused, returns 0
+//Export void __cdecl NoResponseToTrigger();	// Used for triggers with no custom effects
 
