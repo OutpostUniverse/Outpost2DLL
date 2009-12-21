670c06b4ec265e06de67b611ec02f3d1e6839963
Hooman <Hooman@7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8>
2009-12-21 05:54:35 +0000

    Added more comments for MissionTypes enum.
    git-svn-id: https://svn.outpostuniverse.org:8443/svn/outpost2/LevelsAndMods/trunk/API/Outpost2DLL@653 7b3b2116-5498-11dd-abe4-fb2d8bf8a5a8

--- RequiredExports.h
+++ RequiredExports.h
@@ -8,18 +8,20 @@
 #define Export extern "C" __declspec(dllexport)
 
 // Mission types, and the corresponding DLL name prefix
-// Note: For campaign games, use a positive level number.
+// Note: For campaign games, use a positive level number, and a prefix of e (Eden) or p (Plymouth)
+// Note: Multiplayer games include the max number of players (denoted by <x>).
+//   Example: ml6_99.dll  (Multiplayer, Last One Standing, Max of 6 players)
 enum MissionTypes
 {
 	Colony					= -1, //0xFF	// c
 	AutoDemo				= -2, //0xFF	// a
 	Tutorial				= -3, //0xFD	// t
 
-	MultiLandRush			= -4, //0xFC	// mu(x)
-	MultiSpaceRace			= -5, //0xFB	// mf(x)
-	MultiResourceRace		= -6, //0xFA	// mr(x)
-	MultiMidas				= -7, //0xF9	// mm(x)
-	MultiLastOneStanding	= -8, //0xF8	// ml(x)
+	MultiLandRush			= -4, //0xFC	// mu<x>
+	MultiSpaceRace			= -5, //0xFB	// mf<x>
+	MultiResourceRace		= -6, //0xFA	// mr<x>
+	MultiMidas				= -7, //0xF9	// mm<x>
+	MultiLastOneStanding	= -8, //0xF8	// ml<x>
 };
 
 // Structure for important data exports needed for OP2 to recognize the level
